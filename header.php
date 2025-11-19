e4a85ef912f9b4f2104c138d860ae44fd17121fb
Rashedulislam99 <mdrashedulislam604@gmail.com>
2025-11-19 09:30:38 +0600

    height

--- header.php
+++ header.php
@@ -53,6 +53,11 @@ require_once("controllers/controller.php");
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <style>
+            .mb-2{
+                margin-bottom: 0 !important;
+            }
+        </style>
     </head>
     <body class="home-style3">
         
